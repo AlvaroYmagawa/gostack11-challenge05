d36d5896da1e34d7af2db3db6803b0d9540c3592
Kenzo Ymagawa <alvaroymagawa@gmail.com>
2020-07-18 17:47:06 -0300

    finish challenge 05

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('The type is invalid, can only be income or outcome.');
+    } else {
+      const balance = this.transactionsRepository.getBalance();
+
+      if (type === 'outcome' && balance.total < value)
+        throw Error('You have no balance to complete this operation.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
